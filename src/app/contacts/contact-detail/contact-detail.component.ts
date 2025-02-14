78fa83d192d3e25543b1c85d5264c15eb1b1697b
Blahdidah <80852722+Blahdidah@users.noreply.github.com>
2025-02-14 14:00:40 -0700

    update to contact-detail component to load the right contact.

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,11 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
+import { ContactsService } from '../contacts.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-contact-detail',
   templateUrl: './contact-detail.component.html',
   styleUrls: ['./contact-detail.component.css']
 })
-export class ContactDetailComponent {
+export class ContactDetailComponent implements OnInit {
   @Input() contact: Contact;
+
+  constructor(private contactService: ContactsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.contact = this.contactService.getContact(id);
+    })
+  }
 }
