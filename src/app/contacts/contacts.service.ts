09dbf8256d55a3f55403a8fa544981ec48a4c1ae
Blahdidah <80852722+Blahdidah@users.noreply.github.com>
2025-02-19 14:51:18 -0700

    added delete, add, update to contacts

--- src/app/contacts/contacts.service.ts
+++ src/app/contacts/contacts.service.ts
@@ -1,18 +1,21 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Contact } from './contact.model';
 import { MOCKCONTACTS } from './MOCKCONTACTS';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactsService {
-  contactSelectedEvent = new EventEmitter<Contact>();
-  contactChangedEvent = new EventEmitter<Contact[]>();
+  contactSelectedEvent = new Subject<Contact>();
+  contactChangedEvent = new Subject<Contact[]>();
+  private maxContactId = this.getMaxId();
 
   private contacts: Contact[] = [];
 
   constructor() {
     this.contacts = MOCKCONTACTS;
+    this.maxContactId = this.getMaxId();
   }
   
   getContacts() {
@@ -29,7 +32,55 @@ export class ContactsService {
   }
 
   deleteContact(contact: Contact) { 
-    this.contacts = this.contacts.filter(c => c.id !== contact.id);
-    this.contactChangedEvent.emit(this.contacts.slice());
+    if (!contact) {
+      return;
+    }
+    const pos = this.contacts.indexOf(contact);
+    if (pos < 0) {
+      return;
+    }
+    this.contacts.splice(pos, 1);
+    const contactsListClone = this.contacts.slice();
+    this.contactChangedEvent.next(contactsListClone);
   }
+
+  getMaxId(): number {
+    let maxId = 0;
+    for (let contact of this.contacts) {
+      let currentId = parseInt(contact.id, 10);
+      if (!isNaN(currentId) && currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+    return maxId;
+  }
+
+  addContact(newContact: Contact) { 
+    if (!newContact) {
+      return;
+    }
+    this.maxContactId++
+    newContact.id = this.maxContactId.toString();
+
+    this.contacts.push(newContact);
+    const contactsListClone = this.contacts.slice();
+    this.contactChangedEvent.next(contactsListClone);
+  }
+
+  updateContact(originalContact: Contact, newContact: Contact) {
+    if (!originalContact || !newContact) {
+      return;
+    }
+    const pos = this.contacts.indexOf(originalContact);
+    if (pos < 0) {
+      return;
+    }
+
+    newContact.id = originalContact.id;
+    this.contacts[pos] = newContact;
+
+    const contactsListClone = this.contacts.slice();
+    this.contactChangedEvent.next(contactsListClone);
+  }
+
 }
