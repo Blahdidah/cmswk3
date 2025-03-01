958dafa0ac14182ab35ce00bfcef31ef06b9262d
Blahdidah <80852722+Blahdidah@users.noreply.github.com>
2025-02-28 23:32:55 -0700

    as of step 2pt 5 everything is working.

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { document } from '../document.model';
 
 @Component({
   selector: 'app-document-edit',
   templateUrl: './document-edit.component.html',
   styleUrls: ['./document-edit.component.css']
 })
-export class DocumentEditComponent {
+export class DocumentEditComponent implements OnInit{
+  originalDocument: document;
+  document: document;
+  editMode: boolean = false;
 
+  ngOnInit(): void {
+   
+  }
+
+  onSubmit(f) {
+    
+  }
 }
